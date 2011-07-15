3a81ae8443f9c7548c786d5a0b84e10808d5d9b7
Ana Nelson <ana@ananelson.com>
2011-07-15 11:04:31 -0500

    Add beamer/R template.

---
+++ latex-beamer-r/code001.R
@@ -0,0 +1,19 @@
+### @export "assign-variables"
+x <- 6
+y <- 7
+
+### @export "multiply"
+x * y
+
+### @export "graph-name"
+pdf("dexy--plot.pdf", width=6,height=4)
+
+
+### @export "dummy"
+plot(c(1,2,3), pch=19, col="purple")
+
+### @export "graph"
+
+### @export "close-graph"
+dev.off()
+
