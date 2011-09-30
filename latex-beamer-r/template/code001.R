955fcb89b9355787487a31230411bfdc603ac158
Ana Nelson <ana@ananelson.com>
2011-09-29 22:31:33 -0700

    Move code001.py to template directory, add crop region to im-thumb config.

--- latex-beamer-r/template/code001.R
+++ latex-beamer-r/template/code001.R
@@ -8,11 +8,8 @@ x * y
 ### @export "graph-name"
 pdf("dexy--plot.pdf", width=6,height=4)
 
-
-### @export "dummy"
-plot(c(1,2,3), pch=19, col="purple")
-
 ### @export "graph"
+plot(c(1,2,3), pch=19, col="purple")
 
 ### @export "close-graph"
 dev.off()
