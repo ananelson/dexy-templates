6dfd95352375eccf3faef6f5b8b05cbc9ad9a7f1
Ana Nelson <ana@ananelson.com>
2011-10-27 02:20:42 -0700

    Move code-journal-html-python files into template/ dir and generate thumbnail.

--- custom-dexy-filter/filters/my_filter.py
+++
@@ -1,7 +0,0 @@
-from dexy.dexy_filter import DexyFilter
-
-class SillyHandler(DexyFilter):
-    ALIASES =['com.example.silly']
-
-    def process_text(self, input_text):
-        return "you said: '%s'\n that's SOOOO silly!\n" % input_text
--- python-modules/modify-pythonpath/demo-script.py
+++
@@ -1,8 +0,0 @@
-### @export "add-parent-to-pythonpath"
-import sys
-sys.path.append("..")
-
-### @export "call-module"
-import demomodule
-print demomodule.abc()
-
