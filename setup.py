540310190377a1a1150728f6e9036734fbaee9d1
Ana Nelson <ana@ananelson.com>
2012-11-16 20:07:00 -0800

    Add more metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,23 @@ from setuptools import setup, find_packages
 setup(
         author='Ana Nelson',
         author_email='ana@ananelson.com',
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: Financial and Insurance Industry",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Topic :: Documentation",
+            "Topic :: Software Development :: Build Tools",
+            "Topic :: Software Development :: Code Generators",
+            "Topic :: Software Development :: Documentation",
+            "Topic :: Text Processing",
+            "Topic :: Text Processing :: Markup :: HTML",
+            "Topic :: Text Processing :: Markup :: LaTeX"
+            ],
+        description='Templates for dexy projects.',
         include_package_data = True,
         install_requires = [
             'dexy',
@@ -11,7 +28,5 @@ setup(
         name='dexy_templates',
         packages=find_packages(),
         url='http://dexy.it',
-        version="0.0.1"
+        version="0.0.2"
         )
-
-
