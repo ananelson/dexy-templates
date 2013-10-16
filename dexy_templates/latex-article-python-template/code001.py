4fd9adc75f92125d87c16c1df383ec10d69ae406
Ana Nelson <ana@ananelson.com>
2013-10-16 16:56:06 -0700

    Add example of writing data to a JSON file and loading it in a document.

--- dexy_templates/latex-article-python-template/code001.py
+++ dexy_templates/latex-article-python-template/code001.py
@@ -5,3 +5,14 @@ y = 7
 ### @export "multiply"
 x * y
 
+### @export "save-some-json-data"
+
+data = {
+        'foo' : 123,
+        'bar' : "this is a string",
+        'baz' : None
+        }
+
+import json
+with open("data.json", 'wb') as f:
+    json.dump(data, f)
